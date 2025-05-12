e64bd794bfb97f59b8b8db4b1e4f92074b9efd75
GastonRomero269 <114840130+GastonRomero269@users.noreply.github.com>
2025-05-12 12:05:13 -0300

    Repository: Viaje

---
+++ src/main/java/com/unla/tp_ing_sw_3_grupo_6/repository/ViajeRepository.java
@@ -0,0 +1,8 @@
+package com.unla.tp_ing_sw_3_grupo_6.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.ing_sw_3_grupo_6_sube.entity.Viaje;
+
+public interface ViajeRepository extends JpaRepository<Viaje, Long> {
+}
