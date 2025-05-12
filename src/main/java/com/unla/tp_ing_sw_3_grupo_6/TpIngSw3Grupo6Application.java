a384a1295f8bed7751d414277801efe3fa3e85a5
GastonRomero269 <114840130+GastonRomero269@users.noreply.github.com>
2025-05-12 11:54:31 -0300

    Initial commit

---
+++ src/main/java/com/unla/tp_ing_sw_3_grupo_6/TpIngSw3Grupo6Application.java
@@ -0,0 +1,13 @@
+package com.unla.tp_ing_sw_3_grupo_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpIngSw3Grupo6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpIngSw3Grupo6Application.class, args);
+	}
+
+}
